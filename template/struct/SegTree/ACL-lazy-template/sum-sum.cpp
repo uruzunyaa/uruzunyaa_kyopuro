85738ef8ff1831a8c28c5ccfbc2ddcfb501ffe2e
uruzunyaa <uruzunyaa@gmail.com>
2025-12-04 18:24:47 +0900

    ACLを非参照に

--- template/struct/SegTree/ACL-lazy-template/sum-sum.cpp
+++ template/struct/SegTree/ACL-lazy-template/sum-sum.cpp
@@ -5,14 +5,9 @@ using namespace std;
 #define loop(i,m,n) for(long long i=m;i<=(ll)n;i++)
 #define vl vector<long long>
 #define vvl vector<vector<long long>>
-#define vdbg(a) rep(ii,a.size()){cout<<a[ii]<<" ";}cout<<endl;
-#define vvdbg(a) rep(ii,a.size()){rep(jj,a[ii].size()){cout<<a[ii][jj]<<" ";}cout<<endl;}
-#define setdbg(a) for(const auto & ii:a){cout<<ii<<" ";}cout<<endl;
 #define inf 4000000000000000000LL
 #define mod 998244353LL
 //#define mod 1000000007LL
-
-#include"atcoder/lazysegtree.hpp"
 
 struct S{
     long long value;
@@ -27,7 +22,5 @@ F composition(F f, F g){ return f+g; }
 F id(){ return 0; }
 
 int main(){
-    int N;
-    std::vector<S> v(N, {0, 1});
-    atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(v);
+
 }
